b5e7c21be64c8455a53b776e3eacd1aa9648ce84
Mhd-tz <61482609+Mhd-tz@users.noreply.github.com>
2021-12-30 19:17:09 -0800

    V 0.0 (Adding Packages)
    Add the necessary packages to the project

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
     <View>
-      <Text>Hello World</Text>
+      <Text>Hello World2</Text>
     </View>
   );
 }
